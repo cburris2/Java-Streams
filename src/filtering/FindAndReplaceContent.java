403a4f3d1817d7801330122a1fa8660c8ae78f2f
cburris2 <cburris1983@gmail.com>
2025-07-25 23:06:16 -0400

    Update FindAndReplaceContent to work on test.json and add test.json

--- src/filtering/FindAndReplaceContent.java
+++ src/filtering/FindAndReplaceContent.java
@@ -20,10 +20,10 @@ public class FindAndReplaceContent {
 	 */
 	public static void main(String[] args) throws IOException {
 		// find content within the given file and replace its content
-		final Path path = Paths.get("/home/logfile");
+		final Path path = Paths.get("C:/Users/shock/git/Java-Streams/src/filtering/"); // specify the path to the directory where the json files are located
 		final String ext = ".json";
-		final String old = "";
-		final String newText = "";
+		final String old = "path";
+		final String newText = "absolute_path";
 		findAndReplace(path, ext, old, newText);
 
 	}
@@ -31,21 +31,26 @@ public class FindAndReplaceContent {
 	private static void findAndReplace(Path path, String ext, String old, String newText) throws IOException {
 
 		try {
+			System.out.println("Searching for files with extension: " + ext + " in path: " + path);
+			System.out.println(Files.list(path).count() + " files found.");
 			Files.list(path)
-			.filter(f -> f.endsWith(ext))
+			.filter(f -> f.toString().endsWith(ext))
 			.map(Path::getFileName)
 			.forEach(file -> {
 				try {
-					file.resolve(path);
-					String content = new String(Files.readAllBytes(file));
+
+					String content = new String(Files.readAllBytes(path.resolve(file)));
 					content = content.replaceAll(old, newText);
-					Files.write(file, content.getBytes());
+					Files.write(path.resolve(file), content.getBytes());
+					System.out.println("Replaced content in file: " + file);
+					System.out.println("Old content: " + old);
+					System.out.println("New content: " + newText);
 				} catch (final IOException e) {
 					throw new RuntimeException(e);
 				}
 
 			});
-		} catch (final IOException e) {
+		} catch (final Exception e) {
 			throw new RuntimeException(e);
 		}
 
